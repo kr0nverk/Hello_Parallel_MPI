ad1924c81842829a73486c5b799930eb1b066b54
kr0nverk <rusakov.aleksej94@gmail.com>
2020-10-02 00:31:50 +0300

    no message

--- Hello_Parallel_MPI/Hello_Parallel_MPI.cpp
+++ Hello_Parallel_MPI/Hello_Parallel_MPI.cpp
@@ -10,10 +10,7 @@ int main(int *argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
- 
-   
     printf("Hello MPI from process = %d, total number of processes: %d\n", rank, numtasks);
 
- 
     MPI_Finalize();
 }
