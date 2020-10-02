a5e88b6dc70ae7cda5cf04a7f6879f9385a45521
kr0nverk <rusakov.aleksej94@gmail.com>
2020-10-02 17:21:57 +0300

    Pi Monte Carlo method

--- Hello_Parallel_MPI/Hello_Parallel_MPI.cpp
+++ Hello_Parallel_MPI/Hello_Parallel_MPI.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int *argc, char** argv) {
+int main(int argc, char* argv[]) {
 
     int numtasks, rank;
 
-    MPI_Init(argc, &argv);
+    MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-    printf("Hello MPI from process = %d, total number of processes: %d\n", rank, numtasks);
+    printf("Hello MPI from process = %2d, total number of processes: %d\n", rank, numtasks);
 
     MPI_Finalize();
 }
---
+++ Pi_Monte_Carlo_method/Pi_Monte_Carlo_method.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <time.h>
+#include <cmath>
+
+int main(int argc, char* argv[]) {
+	int proc_rank, proc_num;
+	double x_rand, y_rand, start_time, end_time;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	start_time = MPI_Wtime();
+	
+	int n = 16000000;
+	int rank_hit = 0;
+	srand((unsigned)(time(0)));
+
+	int n_proc_rank = n / proc_num;
+	for (int i = 0; i < n_proc_rank; ++i) {
+		x_rand = (double)rand() / (double)RAND_MAX;
+		y_rand = (double)rand() / (double)RAND_MAX;
+		if (x_rand * x_rand + y_rand * y_rand <= 1.0) {
+			rank_hit++;
+		}
+	}
+	int hit = 0;
+	MPI_Reduce(&rank_hit, &hit, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	double monte_carlo_pi = hit * 4 / (double)n;
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	end_time = MPI_Wtime();
+
+	if (proc_rank == 0) {
+		printf("Points: %d\n",n);
+		printf("Pi: %10.5f\n", monte_carlo_pi);
+		printf("Time: %5.3f\n", end_time - start_time);
+	}
+	
+	MPI_Finalize();
+	return 0;
+}
